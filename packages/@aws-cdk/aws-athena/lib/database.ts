64716126b50217b0bdb0fdb2a58d121877f438a3
Erik Snoek <erik.snoek@sogeti.com>
2020-03-24 19:35:30 +0100

    Singleton provider

--- packages/@aws-cdk/aws-athena/lib/database.ts
+++ packages/@aws-cdk/aws-athena/lib/database.ts
@@ -2,7 +2,7 @@ import * as cfn from '@aws-cdk/aws-cloudformation';
 import * as iam from '@aws-cdk/aws-iam';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as s3 from '@aws-cdk/aws-s3';
-import { Construct } from '@aws-cdk/core';
+import { Construct, Stack } from '@aws-cdk/core';
 import * as cr from '@aws-cdk/custom-resources';
 import { FileFormat, RowFormat } from './props';
 
@@ -37,48 +37,13 @@ export class Database extends Construct {
   constructor(scope: Construct, id: string, props: DatabaseProps) {
     super(scope, id);
 
-    const onEvent = new lambda.Function(this, 'DatabaseQueryLambda', {
-      code: new lambda.InlineCode(handlerCode),
-      runtime: lambda.Runtime.PYTHON_3_7,
-      handler: 'index.on_event',
-      initialPolicy: [
-        new iam.PolicyStatement({
-          resources: [ '*' ],
-          actions: [
-            'athena:*'
-          ]
-        }),
-        new iam.PolicyStatement({
-          resources: [ '*' ],
-          actions: [
-            's3:*'
-          ]
-        }),
-        new iam.PolicyStatement({
-          resources: [ '*' ],
-          actions: [
-            'glue:GetDatabase',
-            'glue:CreateDatabase',
-            'glue:DeleteDatabase',
-            'glue:GetTables',
-            'glue:GetTable',
-            'glue:DeleteTable'
-          ]
-        })
-      ]
-    });
-
-    const provider = new cr.Provider(this, 'DatabaseQueryProvider', {
-      onEventHandler: onEvent
-    });
-
     this.name = props.name;
 
     const createQueryString = `CREATE DATABASE IF NOT EXISTS ${props.name}`;
     const deleteQueryString = `DROP DATABASE IF EXISTS ${props.name} CASCADE`;
 
     new cfn.CustomResource(this, 'Resource', {
-      provider,
+      provider: QueryProvider.getOrCreate(this),
       resourceType: 'Custom::AthenaDatabase',
       properties: {
         CreateQueryString: createQueryString,
@@ -162,7 +127,50 @@ export class Table extends Construct {
   constructor(scope: Construct, id: string, props: TableProps) {
     super(scope, id);
 
-    const onEvent = new lambda.Function(this, 'TableQueryLambda', {
+    this.name = props.name;
+
+    const columns = props.columns.map(x => `${x[0]} ${x[1]}`).join(',');
+
+    const createQueryString = `
+      CREATE EXTERNAL TABLE IF NOT EXISTS ${props.databaseName}.${props.name} (${columns})
+      ROW FORMAT ${props.rowFormat.statement}
+      STORED AS ${props.fileFormat.statement}
+      LOCATION 's3://${props.location.bucketName}/${props.locationKey ? props.locationKey + '/' : ''}'
+      TBLPROPERTIES ('has_encrypted_data' = '${props.encryptedData ? 'true' : 'false'}')`;
+
+    const deleteQueryString = `DROP TABLE ${props.databaseName}.${props.name}`;
+
+    new cfn.CustomResource(this, 'Resource', {
+      provider: QueryProvider.getOrCreate(this),
+      resourceType: 'Custom::AthenaTable',
+      properties: {
+        CreateQueryString: createQueryString,
+        UpdateQueryString: createQueryString,
+        DeleteQueryString: deleteQueryString,
+        WorkGroup: props.workGroupName
+      }
+    });
+  }
+}
+
+class QueryProvider extends Construct {
+
+  /**
+   * Returns the singleton provider.
+   */
+  public static getOrCreate(scope: Construct): cr.Provider {
+    const stack = Stack.of(scope);
+    const id = 'com.amazonaws.cdk.athena.query-provider';
+    const x = stack.node.tryFindChild(id) as QueryProvider || new QueryProvider(stack, id);
+    return x.provider;
+  }
+
+  private readonly provider: cr.Provider;
+
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    const onEvent = new lambda.Function(this, 'QueryLambda', {
       code: new lambda.InlineCode(handlerCode),
       runtime: lambda.Runtime.PYTHON_3_7,
       handler: 'index.on_event',
@@ -182,44 +190,14 @@ export class Table extends Construct {
         new iam.PolicyStatement({
           resources: [ '*' ],
           actions: [
-            'glue:GetDatabase',
-            'glue:GetTable',
-            'glue:CreateTable',
-            'glue:DeleteTable',
-            'glue:GetPartitions',
-            'glue:GetPartition',
-            'glue:DeletePartition'
+            'glue:*',
           ]
         })
       ]
     });
 
-    const provider = new cr.Provider(this, 'TableQueryProvider', {
-      onEventHandler: onEvent
-    });
-
-    this.name = props.name;
-
-    const columns = props.columns.map(x => `${x[0]} ${x[1]}`).join(',');
-
-    const createQueryString = `
-      CREATE EXTERNAL TABLE IF NOT EXISTS ${props.databaseName}.${props.name} (${columns})
-      ROW FORMAT ${props.rowFormat.statement}
-      STORED AS ${props.fileFormat.statement}
-      LOCATION 's3://${props.location.bucketName}/${props.locationKey ? props.locationKey + '/' : ''}'
-      TBLPROPERTIES ('has_encrypted_data' = '${props.encryptedData ? 'true' : 'false'}')`;
-
-    const deleteQueryString = `DROP TABLE ${props.databaseName}.${props.name}`;
-
-    new cfn.CustomResource(this, 'Resource', {
-      provider,
-      resourceType: 'Custom::AthenaTable',
-      properties: {
-        CreateQueryString: createQueryString,
-        UpdateQueryString: createQueryString,
-        DeleteQueryString: deleteQueryString,
-        WorkGroup: props.workGroupName
-      }
+    this.provider = new cr.Provider(this, 'QueryProvider', {
+      onEventHandler: onEvent,
     });
   }
 }
